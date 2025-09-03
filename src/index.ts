c791d73f931f076f4809eed3d02e16f0875b61ba
Ayman Alhaik <AymanAlhaikg@Gmail.com>
2025-09-03 18:03:45 +0300

    initial

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express, { Express, Request, Response } from 'express';
+const app: Express = express();
+app.get("/", (req: Request, res: Response) => {
+    res.send("working");
+});
+app.listen(3000, () => {
+    console.log('server on the port: ' + 3000);
+});
