e13af61fa5b3717f547f7ee54d194e4a844c935a
Ayman Alhaik <AymanAlhaikg@Gmail.com>
2025-09-04 11:24:04 +0300

    configuring .env, organizing project structure, configuring auth route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express, { Express, Request, Response } from 'express';
+import { PORT } from './secrets';
+import rootRouter from './routes';
 const app: Express = express();
-app.get("/", (req: Request, res: Response) => {
-    res.send("working");
-});
-app.listen(3000, () => {
+
+app.use('/api', rootRouter);
+
+app.listen(PORT, () => {
     console.log('server on the port: ' + 3000);
 });
